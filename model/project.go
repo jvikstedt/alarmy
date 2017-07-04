3b131a4fe99fc00ba030af6040f0ef84f650390a
Janne Vikstedt <vikstedt.janne@gmail.com>
2017-07-04 13:12:22 +0300

    Simple required validation for project model

--- model/project.go
+++ model/project.go
@@ -10,3 +10,13 @@ type Project struct {
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 }
+
+func (p Project) Errors() map[string][]string {
+	errors := make(map[string][]string)
+
+	if len(p.Name) <= 0 {
+		errors["name"] = append(errors["name"], "is required")
+	}
+
+	return errors
+}
