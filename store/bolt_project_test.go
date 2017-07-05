0fa5817245f759e6c69cade05402ee154acf6365
Janne Vikstedt <vikstedt.janne@gmail.com>
2017-07-05 11:57:13 +0300

    BoltStore ProjectCreate test

--- store/bolt_project_test.go
+++ store/bolt_project_test.go
@@ -2,12 +2,13 @@ package store_test
 
 import (
 	"testing"
+	"time"
 
 	"github.com/jvikstedt/alarmy/model"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestBoltProjectAll(t *testing.T) {
+func TestProjectAll(t *testing.T) {
 	projectStore.ProjectRemoveAll()
 
 	// Zero projects
@@ -22,4 +23,26 @@ func TestBoltProjectAll(t *testing.T) {
 	projects, err = projectStore.ProjectAll()
 	assert.Nil(t, err, "project all should not return an error")
 	assert.Equal(t, 1, len(projects), "should be 1 project")
+}
+
+func TestProjectCreate(t *testing.T) {
+	projectStore.ProjectRemoveAll()
+
+	testProjects := []model.Project{
+		model.Project{Name: "Golang"},
+		model.Project{Name: "Ruby"},
+		model.Project{Name: "Javascript"},
+	}
+
+	for i, p := range testProjects {
+		project, err := projectStore.ProjectCreate(p)
+
+		assert.Nil(t, err, "ProjectCreate should not return an error")
+		assert.Equal(t, i+1, project.ID, "id should be the same")
+		assert.Equal(t, p.Name, project.Name, "project name should be same")
+		duration := time.Since(project.CreatedAt).Seconds()
+		assert.True(t, duration > 0 && duration < 1, "duration since created at should be between 0 and 1 seconds")
+		duration = time.Since(project.UpdatedAt).Seconds()
+		assert.True(t, duration > 0 && duration < 1, "duration since updated at should be between 0 and 1 seconds")
+	}
 }
