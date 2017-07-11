c6ff6cf059ea2231761880d4df7b77de674486ab
Janne Vikstedt <vikstedt.janne@gmail.com>
2017-07-11 12:42:27 +0300

    project list command to list all projects

---
+++ service/service.go
@@ -0,0 +1,37 @@
+package service
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"time"
+
+	"github.com/jvikstedt/alarmy/model"
+)
+
+const BASE_URL = "http://localhost:8080"
+
+var client = &http.Client{
+	Timeout: time.Second * 10,
+}
+
+func ProjectAll() ([]model.Project, error) {
+	var projects []model.Project
+
+	req, _ := http.NewRequest("GET", fmt.Sprintf("%s/projects", BASE_URL), nil)
+	resp, err := client.Do(req)
+	if err != nil {
+		return projects, err
+	}
+	defer resp.Body.Close()
+
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return projects, err
+	}
+
+	err = json.Unmarshal(data, &projects)
+
+	return projects, err
+}
