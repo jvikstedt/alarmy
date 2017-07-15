13a753f16a8bc365200768c3440f37a83a1db754
Janne Vikstedt <vikstedt.janne@gmail.com>
2017-07-15 14:57:41 +0300

    cmd/server cleaning up logging & output and comments

--- cmd/server.go
+++ cmd/server.go
@@ -26,49 +26,61 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
+		// Store / Database Setup
 		boltStore, err := store.NewBoltStore("alarmy_dev.db")
 		if err != nil {
-			panic(err)
+			printAndExitError(err, nil)
 		}
 		defer boltStore.Close()
 
+		// Logger setup
 		f, err := os.OpenFile("dev.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 		if err != nil {
-			panic(err)
+			printAndExitError(err, nil)
 		}
 		defer f.Close()
+		logger := log.New(f, "", log.LstdFlags)
 
+		// Scheduler setup
 		scheduler := schedule.NewCronScheduler()
 		go scheduler.Start()
 		defer scheduler.Stop()
 
-		logger := log.New(f, "", log.LstdFlags)
-
+		// Server & http.Handler setup
 		api := api.NewApi(boltStore.Store(), logger, scheduler)
 		handler, err := api.Handler()
 		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
+			printAndExitError(err, logger)
 		}
+		s := http.Server{Addr: ":8080", Handler: handler}
 
+		// Signalling
 		stop := make(chan os.Signal, 1)
 		signal.Notify(stop, os.Interrupt)
-
-		s := http.Server{Addr: ":8080", Handler: handler}
 
 		go func() {
 			<-stop
 
 			ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 			defer cancel()
+
+			// Shutdown server gracefully
 			s.Shutdown(ctx)
 		}()
 
+		// Server startup
 		if err := s.ListenAndServe(); err != nil {
-			fmt.Println(err)
-			os.Exit(1)
+			printAndExitError(err, logger)
 		}
 	},
+}
+
+func printAndExitError(a interface{}, logger *log.Logger) {
+	if logger != nil {
+		logger.Println(a)
+	}
+	fmt.Println(a)
+	os.Exit(1)
 }
 
 func init() {
