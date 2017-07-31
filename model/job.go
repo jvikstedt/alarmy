fd04ac982cb0d2df1e429771cf0a114e762b4d74
Janne Vikstedt <vikstedt.janne@gmail.com>
2017-07-31 10:20:23 +0300

    Trigger stuff to its own file

--- model/job.go
+++ model/job.go
@@ -2,26 +2,6 @@ package model
 
 import "time"
 
-const (
-	TriggerEqual    = 0
-	TriggerLessThan = 1
-	TriggerMoreThan = 2
-)
-
-type TriggerType int
-
-var TriggerTypes = []string{
-	"Equal",
-	"Less Than",
-	"More Than",
-}
-
-type Trigger struct {
-	FieldName string
-	Target    string
-	TriggerType
-}
-
 type Job struct {
 	ID        int       `json:"id"`
 	Name      string    `json:"name"`
